94d03093d411b7e2c69ecb712b428d5c6e1eb975
Michael Totschnig <michael@totschnig.org>
2024-11-22 08:53:46 +0100

    Quick fix for edge-to-edge display on Android 15

--- app/src/main/java/org/totschnig/ocr/SettingsActivity.kt
+++ app/src/main/java/org/totschnig/ocr/SettingsActivity.kt
@@ -2,6 +2,9 @@ package org.totschnig.ocr
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceScreen
 
@@ -11,11 +14,24 @@ class SettingsActivity : AppCompatActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { v, insets ->
+            val bars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars()
+                        or WindowInsetsCompat.Type.displayCutout()
+            )
+            v.updatePadding(
+                left = bars.left,
+                top = bars.top,
+                right = bars.right,
+                bottom = bars.bottom,
+            )
+            WindowInsetsCompat.CONSUMED
+        }
     }
 
     override fun onPreferenceStartScreen(
         caller: PreferenceFragmentCompat,
-        pref: PreferenceScreen
+        pref: PreferenceScreen,
     ): Boolean {
         startPreferenceScreen(pref.key)
         return true
